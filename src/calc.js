8e835da3ae8d0619042667d30836429a47d23d60
Hotwings <edcorgiat@gmail.com>
2024-08-12 10:47:45 -0500

    We can now load all resources into an array

--- src/calc.js
+++ src/calc.js
@@ -1,1 +1,52 @@
-console.log("hello");
+
+const apiUrl = "https://west.albion-online-data.com"
+
+var minimumTier = 4;
+var maxTier = 8;
+
+var minimumQuality = 1;
+var maxQuality = 1;
+
+const HIDE_NAME = "HIDE"
+const LEATHER_NAME = "LEATHER"
+
+const FIBER_NAME = "FIBER"
+const CLOTH_NAME = "CLOTH"
+
+const WOOD_NAME = "WOOD"
+const PLANKS_NAME = "PLANKS"
+
+const ROCK_NAME = "ROCK"
+const STONE_NAME = "STONE"
+
+const ORE_NAME = "ORE"
+const METALBAR_NAME = "METALBAR"
+
+
+main();
+async function main() {
+	var resourcesList = "";
+	for (var i = minimumTier; i <= maxTier; i++) {
+		resourcesList += "T" + i + "_" + HIDE_NAME + ",";
+		resourcesList += "T" + i + "_" + LEATHER_NAME + ",";
+		resourcesList += "T" + i + "_" + FIBER_NAME + ",";
+		resourcesList += "T" + i + "_" + CLOTH_NAME + ",";
+		resourcesList += "T" + i + "_" + WOOD_NAME + ",";
+		resourcesList += "T" + i + "_" + PLANKS_NAME + ",";
+		resourcesList += "T" + i + "_" + ROCK_NAME + ",";
+		resourcesList += "T" + i + "_" + STONE_NAME + ",";
+		resourcesList += "T" + i + "_" + ORE_NAME + ",";
+		resourcesList += "T" + i + "_" + METALBAR_NAME + ",";
+	}
+	resourcesList = resourcesList.slice(0, -1);
+	var qualitiesList = "";
+	for (var i = minimumQuality; i <= maxQuality; i++) {
+		qualitiesList+=i+",";
+	}
+	qualitiesList = qualitiesList.slice(0, -1);
+	const response = await fetch(apiUrl + "/api/v2/stats/Prices/" + resourcesList + ".json?locations=Bridgewatch,Fort Sterling,Lymhurst,Martlock,Thetford&qualities="+qualitiesList)
+	const responseJson = await response.json();
+	console.log(responseJson);
+
+	
+}
